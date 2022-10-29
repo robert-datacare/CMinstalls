f75daae0ebe9af50fa8d9babeb6007baf969252d
robert67976 <108817546+robert67976@users.noreply.github.com>
2022-10-29 15:31:31 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install ubuntu-drivers-common
+sudo apt-get update
+sudo apt-get install openssh-server
+sudo ubuntu-drivers install 510
+sudo apt install nvidia-driver-510 nvidia-dkms-510
